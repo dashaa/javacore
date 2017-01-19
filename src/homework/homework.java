dc5ed20666d396418549b161cc23df3588917bdc
Daria Kovtun <daryakovtun2@gmail.com>
2017-01-19 19:24:36 +0200

    edited gitignore pom.xml and src

---
+++ src/homework/homework.java
@@ -0,0 +1,10 @@
+package homework;
+
+import java.util.Scanner;
+public class homework {
+    public static void main (String [] args)
+    {
+       Scanner scanner = new Scanner (System.in);
+        System.out.println(scanner.nextLine());
+    }
+}
